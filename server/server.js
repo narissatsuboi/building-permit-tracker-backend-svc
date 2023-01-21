0eb687cbe84067aa59bd5c6f2f546b60e464bc58
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-01-20 20:50:33 -0800

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,38 @@
+// Loads the configuration from config.env to process.env
+require('dotenv').config({ path: './config.env' });
+
 const express = require('express');
+const cors = require('cors');
+// get MongoDB driver connection
+// const dbo = require('./db/conn');
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+// app.use(require('./routes/record'));
 
-// run server 
-const startServer = async () => {
-    try {
-        app.listen(port, () => console.log(`Server listening on port ${port}...`));
-    } catch (error) {console.log(error);}
-};
+// Global error handling
+app.use(function (err, _req, res) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
-startServer(); 
+// perform a database connection when the server starts
+// dbo.connectToServer(function (err) {
+//   if (err) {
+//     console.error(err);
+//     process.exit();
+//   }
+
+//   // start the Express server
+//   app.listen(PORT, () => {
+//     console.log(`Server is running on port: ${PORT}`);
+//   });
+// });
+
+// start the Express server
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
