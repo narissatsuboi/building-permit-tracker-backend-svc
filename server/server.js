59538ae3c888b666850138622cbeb6f8985244a1
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-01-23 12:09:25 -0800

    Added use statements for middleware to server.js

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(cookieParser());
 app.use(require('./routes/record'));
 
 // Global error handling
