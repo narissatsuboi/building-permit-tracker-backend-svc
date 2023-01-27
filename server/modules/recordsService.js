49f7275805e10036d5325e7de97598f92b584026
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-01-26 20:42:04 -0800

    implemented getdata from external api

--- server/modules/recordsService.js
+++ server/modules/recordsService.js
@@ -1,28 +1,24 @@
 import axios from 'axios';
 import * as fs from 'fs';
 
-const config = {
-    headers:{
-        'X-App-Token':'g1akUYTinxtDHh4gfHYFRQjr8'
-    }
+const BASE_URL = 'https://data.nola.gov/resource/rcm3-fn58.json';
+const X_APP_TOKEN = 'g1akUYTinxtDHh4gfHYFRQjr8';
+
+const URL = 'https://data.nola.gov/resource/rcm3-fn58.json?$limit=10';
+const PATH = 'records.json';
+
+async function getData() {
+  const res = await axios.get(URL);
+  return res.data;
 }
 
-function getData() {
-    axios.get('https://data.nola.gov/resource/rcm3-fn58.json?$limit=1', config)
-    .then(function (response) {
-      // handle success
-    //   console.log(response);
-        console.log(typeof(response));
-    })
-    .catch(function (error) {
-      // handle error
+async function writeData(path, data) {
+  fs.writeFile(path, data, (error) => {
+    if (error) {
       console.log(error);
-    })
-    .then(function (response) {
-      // always executed
-      return response;
-    });
-}
+      return;
+    }
+  })
+};
 
-let res = getData();
-console.log();
+getData().then(data => writeData(PATH, JSON.stringify(data)));
