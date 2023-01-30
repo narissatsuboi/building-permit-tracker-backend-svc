dea5e193428729fb53e43617af9951e261375575
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-01-29 21:19:27 -0800

    added  env and logging constants

---
+++ src/common/constants/index.js
@@ -0,0 +1,9 @@
+const PRODUCTION_ENV = 'production';
+const VERBOSE_LOGGING_LVL = 'verbose';
+const INFO_LOGGING_LVL = 'info';
+
+module.exports = {
+    PRODUCTION_ENV,
+    VERBOSE_LOGGING_LVL,
+    INFO_LOGGING_LVL,
+};
