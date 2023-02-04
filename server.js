0c54ec0d7740ca98de3369798607ae8fd7fa0f46
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-02-03 23:13:38 -0800

    replace body-parser with express.json

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const corsConfig = require('./configuration/corsConfig');
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const errorHandler = require(path.join(__dirname, 'presentation', 'middleware', 'errorHandler'));
 const { logger, requestLogger, errorLogger } = require(path.join(__dirname, 'common', 'logging', 'index.js'))
@@ -24,8 +23,7 @@ app.use(requestLogger);
 app.use(errorLogger);
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded());
 app.use(cookieParser());
 
 // routes
