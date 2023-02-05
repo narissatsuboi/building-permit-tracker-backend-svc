f05d4fb6afe7b901b21c9bba6e9e41219fce39d7
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-02-04 23:21:23 -0800

    refactor access to db uri

--- configuration/dbConn.js
+++ configuration/dbConn.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const CLUSTER = 'cluster0';
+const DB_NAME = 'tracki';
+
+let MONGO_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${CLUSTER}.j7kdj7e.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
+        await mongoose.connect(MONGO_URL, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
@@ -10,5 +14,7 @@ const connectDB = async () => {
         console.log(err);
     }
 };
-
-module.exports = connectDB;
+module.exports = {
+    connectDB,
+    MONGO_URL
+};
