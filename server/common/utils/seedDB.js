9bac50754346e5cf32719ab76145b84f52a6da91
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-02-10 01:22:42 -0800

    add time range selection in seedDB

--- server/common/utils/seedDB.js
+++ server/common/utils/seedDB.js
@@ -3,10 +3,26 @@ const fetch = require("node-fetch");
 const axios = require("axios");
 const { Record, conn } = require(path.join("..", "..", "schemas", "Record"));
 
+/**
+ * Helper function to format DateISO string to API
+ * floating point timestamp format. 
+ */
+function getEndDate() {
+  const ESC = '\''; 
+  const date = new Date().toISOString().replace('Z', ''); 
+  return `${ESC}${date}${ESC}`
+}
+
+/**
+ * Utility function to seed DB with preset range of documents. 
+ */
 async function seedDB() {
+  let startDate = '\'2022-01-01T00:00:00.000\'';
+  let endDate = getEndDate(); 
+
   try {
     const response = await axios.get(
-      "https://data.nola.gov/resource/rcm3-fn58.json"
+      `https://data.nola.gov/resource/rcm3-fn58.json?$where=filingdate between ${startDate} and ${endDate}`
     );
     const records = await response.data;
     await Record.insertMany(records, {
@@ -22,4 +38,3 @@ async function seedDB() {
   }
 }
 seedDB();
-// last seeded 2/9/23
