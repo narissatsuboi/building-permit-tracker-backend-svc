4e03ef1b380957594820756f39291ee17e4835d4
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-02-17 17:07:28 -0800

    update records routes

--- server/routes/records.js
+++ server/routes/records.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const fs = require('fs');
+const RecordController = require(path.join('..', 'controllers', 'records'));
 
-router.get('/', (req, res) => {
-    res.send('<h3>RECORDS DASHBOARD</h3>');
-});
+router.get('/', RecordController.getRecordsHandler);
+router.get('/:permitnumber', RecordController.getRecordByPermitNumberHandler);
 
 module.exports = router;
