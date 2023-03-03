b3a269f86fc76cfce44a0b5373e64276117ee4bf
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-03-02 22:35:39 -0800

    update router

--- app/src/router.js
+++ app/src/router.js
@@ -4,19 +4,28 @@ import {
     Route,
   } from 'react-router-dom';
 
+// pages 
+import Home from './pages/Home'
+import Records, { recordsLoader } from './pages/records/Records'
+
+
 // layouts 
 import RootLayout from './layouts/rootLayout'
+import RecordsLayout from './layouts/recordsLayout';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<RootLayout />}>
-            <Route index element={<h1>home component</h1>}/>
+            <Route index element={<Home />}/>
             <Route path='about' element={<h1>about tracki</h1>}/>
             <Route path='faq' element={<h1> faq </h1>}/>
             <Route path='contact' element={<h1>contact us</h1>}/>
 
-                <Route path='records' element={<h1>records</h1>}>
-                    <Route path='records/:permitnumber' element={<h1>single permit card</h1>}/>
+                <Route path='records' element={<RecordsLayout />}>
+                    <Route 
+                    index
+                    element={<Records />}/>
+                    loader={recordsLoader}
                 </Route>
         </Route>
     )
