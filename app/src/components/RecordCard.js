4b57a09213d5fc425e2982e06312a1927a7be06e
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-03-14 14:29:15 -0700

    add basic card

---
+++ app/src/components/RecordCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Image, Flex, Text, Box, Link, Card } from 'theme-ui'
+
+const RecordCard = (props) => {
+  return (
+    <Card>
+      <Text>Card</Text>
+    </Card>
+  )
+}
+
+export default RecordCard
--- app/src/pages/records/Record.js
+++ app/src/pages/records/Record.js
@@ -1,35 +1,67 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom"
-// import { Flex, Text, Box, Link } from "rebass"
-import { Box, Label, Input, Button } from 'theme-ui'
+/** @jsxImportSource theme-ui */
+import React from 'react'
+import { useLoaderData } from 'react-router-dom'
+import { Grid, Card, Text, Box, Label, Input, Button } from 'theme-ui'
+
+import RecordCard from '../../components/RecordCard'
 
 import axios from 'axios'
 
-export default function Record() {
+// export default function Record () {
+//   let records = useLoaderData()
+//   return (
+//     <div>
+//       {records.data.map(record => (
+//         <Box
+//           p={5}
+//           width={[1, 1, 1 / 2]}
+//           // color='black'
+//           bg='pink'
+//           key={record.numstring}
+//         >
+//           {record.address}
+//           {record.numstring}
+//         </Box>
+//       ))}
+//     </div>
+//   )
+// }
+
+export default function Record () {
     let records = useLoaderData()
     return (
-        <div>
-          {records.data.map(record => (
-            <Box 
-                p={5}
-                width={[ 1, 1, 1/2 ]}
-                // color='black'
-                bg='pink'
-                key={record.numstring}
-            >
-                {record.address}
-                {record.numstring}
-            </Box>
-          ))}
-        </div>
-      )
-    }
+      <div>
+        {records.data.map(record => (
+            <Card 
+                sx ={{
+                    maxWidth: 256,
+                    height: 256,
+                    border: 'solid',
+                    borderRadius: 5,
+                }}>
+                <Grid 
+                    key={record.numstring}
+                    sx={{
+                        gap: 2, 
+                        columns: 1,
+                    }}> 
+                    <Box>{record.numstring}</Box>
+                    <Box>{record.address}</Box>
+                    <Box>{record.currentstatus}</Box>
+                    <Box>{record.nextstatus}</Box>
+                </Grid>
+            </Card>
+
+        ))}
+      </div>
+    )
+  }
 
 export const loader = async () => {
-    try {
-        const res = await axios.get('http://localhost:5000/records')
-        return res.data
-    } catch(error) {
-        console.log(error);
-    }
+  try {
+    const res = await axios.get('http://localhost:5000/records')
+    return res.data
+  } catch (error) {
+    console.log(error)
+  }
 }
