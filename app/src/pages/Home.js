104c1619975654694c3854adf4e317cc0a62ea18
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-03-13 22:51:20 -0700

    add logo to nav

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 import { useLoaderData } from "react-router-dom"
-import { Flex, Text, Box, Link } from "rebass";
+// import { Flex, Text, Box, Link } from "rebass";
+import { Image, Flex, Text, Box, Link } from "theme-ui";
 
 export default function Home() {
 
     return(
         <div>
             <Text>home.js page</Text>
-
         </div>
     )
 }
