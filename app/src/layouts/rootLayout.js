6c670b0fb32a87e0a47436481012f7d323ca852c
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-04-19 21:40:26 -0700

    add error page

--- app/src/layouts/rootLayout.js
+++ app/src/layouts/rootLayout.js
@@ -1,32 +1,15 @@
 import * as React from 'react'
 import { Outlet } from 'react-router-dom'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
 import Navbar from '../components/layout/Navbar'
-import Footer from '../components/layout/Footer'
-import CssBaseline from '@mui/material/CssBaseline'
-import Grid from '@mui/material/Grid'
 import Container from '@mui/material/Container'
-import theme from '../themes/theme'
 
-const RootLayout = () => {
+export default function RootLayout() {
   return (
-    // <ThemeProvider theme={theme}>
-      // <CssBaseline />
       <Container maxWidth={false} disableGutters>
         <Navbar></Navbar>
         <main>
           <Outlet></Outlet>
         </main>
       </Container>
-    // </ThemeProvider>
   )
-}
-
-export default RootLayout
-
-{
-  /* <div>
-<Outlet></Outlet>
-<Footer></Footer>
-</div> */
 }
