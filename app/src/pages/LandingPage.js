dcc1a4dfa133a441aee524c409532165893094da
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-04-22 22:08:06 -0700

    remove old landing page text

--- app/src/pages/LandingPage.js
+++ app/src/pages/LandingPage.js
@@ -4,37 +4,13 @@ import Grid from '@mui/material/Unstable_Grid2'
 import { Container, Box, Typography } from '@mui/material'
 import { Card } from '@mui/material'
 import { Image } from 'mui-image'
-import LandingStrip from '../components/index/LandingStrip.jsx'
+import ExcavatorBanner from '../components/index/ExcavatorBanner.jsx'
 
-export default function Home() {
+export default function LandingPage() {
 
     return(
         <Box disableGutters>
-            {/* <Container disableGutters>
-                <Grid 
-                    container
-                    alignItems="center"
-                    justify="center"
-                >
-                    <Grid item xs={2}></Grid>
-                    <Grid item xs={8} sx={{textAlign: "center"}}>
-                    <Billboard></Billboard>
-                </Grid>
-                <Grid item xs={2}> </Grid>
-            </Grid>
-            </Container>
-            <Container disableGutters sx={{mt: 4}}>
-                <Grid 
-                    container
-                    alignItems="center"
-                    justify="center"
-                >
-                    <Grid item xs={12} sx={{textAlign: "center"}}>
-                        Clever description of what tracki does here
-                    </Grid>
-                </Grid>
-            </Container> */}
-            <LandingStrip></LandingStrip>
+            <ExcavatorBanner></ExcavatorBanner>
        </Box>
    )
 }
