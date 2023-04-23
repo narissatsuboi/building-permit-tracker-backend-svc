456af6644094675f7170bcbc20cd2dda5e3e1c2d
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-04-22 20:05:14 -0700

    update navbar text

--- app/src/components/layout/Navbar.js
+++ app/src/components/layout/Navbar.js
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Get Started', 'Records', 'FAQ'];
+const pages = ['About', 'Support'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar() {
