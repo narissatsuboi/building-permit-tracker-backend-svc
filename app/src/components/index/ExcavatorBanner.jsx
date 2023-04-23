5a2cc5c104808cf11c29d25f7fb0c2b3bcd9e9d4
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-04-23 01:44:27 -0700

    basic button

--- app/src/components/index/ExcavatorBanner.jsx
+++ app/src/components/index/ExcavatorBanner.jsx
@@ -1,28 +1,34 @@
 import * as React from 'react'
 import excavImg from '../../assets/excavator.jpg'
 import { Box } from '@mui/material'
+import { Button } from '@mui/material'
+
 import { Typography } from '@mui/material'
 
-
-export default function ExcavatorBanner() {
+export default function ExcavatorBanner () {
   return (
-    // <Box display="flex" alignItems="center" justifyContent="center">
-    <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%'}}>
-      <Box sx={{ // display, align,justify are for the children of this Box, not the Box itself
-                    display: 'flex',
-                    alignItems: 'end', // aligns the text
-                    justifyContent: 'center',
-                    position: 'relative',
-                    backgroundImage:`url(${excavImg})`, backgroundRepeat:'no-repeat',
-                    backgroundSize:'cover',
-                    height: 475, width: 350, maxWidth: 450,
-                  }}>
-        <Typography variant="h5" sx={{position: 'relative', bottom:'50px', left:'75px', color:'black', fontWeight:800}}>
-          Dig efficiency?
-        </Typography>
-      </Box>
+    <Box
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%'
+      }}
+    >
+      <Box
+        sx={{
+          // display, align,justify are for the children of this Box, not the Box itself
+          display: 'flex',
+          alignItems: 'end', // aligns the text
+          justifyContent: 'center',
+          position: 'relative',
+          backgroundImage: `url(${excavImg})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          height: 475,
+          width: 350
+        }}
+      ></Box>
     </Box>
-  );
+  )
 }
-
-
