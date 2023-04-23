91813a92a2ea0a5a98b997c3ed287593790a233f
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-04-22 22:08:44 -0700

    excavation banner

--- app/src/components/index/ExcavatorBanner.jsx
+++ app/src/components/index/ExcavatorBanner.jsx
@@ -1,17 +1,17 @@
 import * as React from 'react'
-import { Container } from '@mui/material'
-import { Button } from '@mui/material'
-import { Card, CardMedia, CardContent, CardActions } from '@mui/material'
-import Typography from '@mui/material/Typography';
-import { Box } from '@mui/material'
 import excavImg from '../../assets/excavator.jpg'
 
-export default function LandingStrip() {
+
+
+export default function ExcavatorBanner() {
   return (
     <div style={{ backgroundImage: `url(${excavImg})`, backgroundRepeat:'no-repeat',
                   backgroundSize:'contain', 
                   height: 550,
                 }}>
+      <p style={{m:0}}>Automated permit tracking</p>
     </div>
   );
 }
+
+
