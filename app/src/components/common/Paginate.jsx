9da6979612569788a9f195437e596970a72083b0
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-05-17 20:40:23 -0700

    justify grid items on center

--- app/src/components/common/Paginate.jsx
+++ app/src/components/common/Paginate.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import { Pagination } from '@mui/material'
 import usePagination from './usePagination' // custom hook
 import PermitCard from '../PermitCard'
 
 export default function Paginate ({ data }) {
   const [currentPage, setCurrentPage] = useState(1)
-  const PER_PAGE = 16
+  const PER_PAGE = 24
   const count = Math.ceil(data.length / PER_PAGE)
   const PAGINATED_DATA = usePagination(data, PER_PAGE)
 
@@ -24,12 +24,15 @@ export default function Paginate ({ data }) {
         height: '100vh',
       }}
       >
+      {/* Card Grid */}
       <Grid
         container
         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
-        sx = {{
-          justifyContent: 'flex-start',
+        sx={{
+          border: '3px solid red',
+          justifyContent: 'center',
+      
         }}
       >
         {PAGINATED_DATA.currentData().map(record => {
@@ -40,9 +43,9 @@ export default function Paginate ({ data }) {
           )
         })}
       </Grid>
+      {/* Pagination */}
       <Box sx={{ 
         display: 'grid',
-        alignItems: 'center',
         justifyContent: 'center',
         }}>
         <Pagination
