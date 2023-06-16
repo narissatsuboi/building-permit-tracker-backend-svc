595102e52ef194606f0aca8b4526fc84be9317f8
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-06-16 15:25:44 -0700

    moved app/ to separate repo

--- app/src/components/common/ContainedBtn.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import { Button } from '@mui/material'
-import { styled } from '@mui/material/styles'
-
-export default function ContainedButton() {
-    return (
-        <Button variant="contained">DEMO</Button>
-    )
-}
